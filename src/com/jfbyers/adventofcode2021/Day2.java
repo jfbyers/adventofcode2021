7667ff8c0982fab9681ab4d412e914b4cbc9d822
Aguado, Eduardo <eaguado@netsuite.com>
2021-12-02 10:23:32 +0100

    Day 2 refactor

--- src/com/jfbyers/adventofcode2021/Day2.java
+++ src/com/jfbyers/adventofcode2021/Day2.java
@@ -3,53 +3,32 @@ package com.jfbyers.adventofcode2021;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
 public class Day2 {
 
-    private final static List<Instruction> PROGRAM = parseProgram();
+    public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args){
-        int horizontal = 0,  depth = 0 , aim = 0;
-        for (Instruction i : PROGRAM){
-            switch (i.op){
-                case "forward":
-                    horizontal += i.units;
-                    depth += aim * i.units;
-                    break;
-                case "down":
-                    aim += i.units;
-                    break;
-                case "up":
-                    aim -= i.units;
-                    break;
-            }
-        }
-        System.out.println(horizontal * depth);
-    }
-    private static List<Instruction> parseProgram() {
-        try {
+            AtomicInteger horizontal = new AtomicInteger(), depth = new AtomicInteger(), aim =  new AtomicInteger();
             final Stream<String> lines = Files.lines(Paths.get("Day2-1.txt"));
-            return lines.map(line -> {
+            lines.forEach(line -> {
                 String[] splitted = line.split(" ");
-                return new Instruction(splitted[0], splitted[1]);
-            }).collect(Collectors.toUnmodifiableList());
-        } catch (IOException e) {
-            //ignored
-        }
-        return Collections.emptyList();
-    }
-
-    private static class Instruction {
-        String op;
-        int units;
-
-        public Instruction(String operand, String units) {
-            this.op = operand;
-            this.units = Integer.valueOf(units);
-        }
+                String operand = splitted[0];
+                int units = Integer.valueOf(splitted[1]);
+                switch (operand) {
+                    case "forward":
+                        horizontal.addAndGet(units);
+                        depth.addAndGet ( aim.get() * units);
+                        break;
+                    case "down":
+                        aim.addAndGet(units);
+                        break;
+                    case "up":
+                        aim.addAndGet(-units);
+                        break;
+                }
+            });
+            System.out.println(horizontal.get() * depth.get());
     }
 }
