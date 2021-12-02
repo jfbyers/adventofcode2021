7ab91bda37fd5794415c35efbd938bebe5bb843e
Aguado, Eduardo <eaguado@netsuite.com>
2021-12-02 10:40:58 +0100

    Day 2 refactor

--- src/com/jfbyers/adventofcode2021/Day2.java
+++ src/com/jfbyers/adventofcode2021/Day2.java
@@ -9,26 +9,38 @@ import java.util.stream.Stream;
 public class Day2 {
 
     public static void main(String[] args) throws IOException {
-
-            AtomicInteger horizontal = new AtomicInteger(), depth = new AtomicInteger(), aim =  new AtomicInteger();
-            final Stream<String> lines = Files.lines(Paths.get("Day2-1.txt"));
-            lines.forEach(line -> {
-                String[] splitted = line.split(" ");
-                String operand = splitted[0];
-                int units = Integer.valueOf(splitted[1]);
-                switch (operand) {
-                    case "forward":
-                        horizontal.addAndGet(units);
-                        depth.addAndGet ( aim.get() * units);
-                        break;
-                    case "down":
-                        aim.addAndGet(units);
-                        break;
-                    case "up":
-                        aim.addAndGet(-units);
-                        break;
-                }
-            });
-            System.out.println(horizontal.get() * depth.get());
+        resolve(1);
+        resolve(2);
     }
+
+    private static void resolve(int part) throws IOException {
+        AtomicInteger horizontal = new AtomicInteger(), aim = new AtomicInteger(), depth = new AtomicInteger();
+        final Stream<String> lines = Files.lines(Paths.get("Day2-1.txt"));
+
+        lines.forEach(line -> {
+            String[] split = line.split(" ");
+            String operand = split[0];
+            int units = Integer.valueOf(split[1]);
+            switch (operand) {
+                case "forward":
+                    horizontal.addAndGet(units);
+                    if (part == 2) depth.addAndGet(aim.get() * units);
+                    break;
+                case "down":
+                    if (part == 2)
+                        aim.addAndGet(units);
+                    else
+                        depth.addAndGet(units);
+                    break;
+                case "up":
+                    if (part == 2)
+                        aim.addAndGet(-units);
+                    else
+                        depth.addAndGet(-units);
+                    break;
+            }
+        });
+        System.out.println("Part " + part + " : " + horizontal.get() * depth.get());
+    }
+
 }
